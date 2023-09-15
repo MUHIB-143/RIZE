bdee7727678d936b0d8994a3d9893c757a7dda6c
MARUFUL HAQUE MUHIB <122245772+MUHIB-143@users.noreply.github.com>
2023-09-15 11:52:31 +0600

    Update run.c

--- run.c
+++ run.c
@@ -5,7 +5,7 @@ int main() {
     printf("RUNNING TOOL...\n");
     
     // Use the system() function to run the Python script
-    system("git clone https://github.com/MUHIB-143/RIZE.git")
+    system("git clone https://github.com/MUHIB-143/RIZE.git");
     int returnCode = system("python RIZE.py");
     
     // Check the return code
